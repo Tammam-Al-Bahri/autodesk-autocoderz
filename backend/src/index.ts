893b75c4ddb9b2f276ffdd9ca49e91d1c720a567
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2026-02-08 23:07:28 +0000

    feat(backend): set up cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,16 @@
 import * as express from "express";
+import * as cors from "cors";
 import usersRouter from "./routes/users";
 
 const app = express();
+
+app.use(
+    cors({
+        origin: true,
+        credentials: false,
+    }),
+);
+
 app.use("/api/users", usersRouter);
 
 const PORT = 3000;
