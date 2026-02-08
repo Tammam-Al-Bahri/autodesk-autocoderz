3cd18446d79f8738249da6041a876a519046f90a
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2026-02-08 20:47:37 +0000

    feat: integrate Prisma with PostgreSQL, testing stuff
    - Added Prisma dependencies to package.json for PostgreSQL support.
    - Created prisma.config.ts for Prisma configuration.
    - Defined database schema in schema.prisma with a Test model.
    - Implemented Prisma client in prisma.ts for database interactions.
    - Updated getUsers handler to create a Test record and return it.
    - Adjusted tsconfig.json to comment out rootDir

--- backend/src/handlers/users.ts
+++ backend/src/handlers/users.ts
@@ -1,6 +1,12 @@
 import type { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
 
-export function getUsers(request: Request, response: Response) {
-    response.send([]);
+export async function getUsers(request: Request, response: Response) {
+    const test = await prisma.test.create({
+        data: {
+            name: `TEST ${Math.random()}`,
+        },
+    });
+    response.send(test);
     return;
 }
