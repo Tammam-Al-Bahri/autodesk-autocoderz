19337d519196f590d48641b81038e0b1401ffc5c
Hamad <hamadabdul149@yahoo.com>
2026-02-17 12:32:42 +0000

    added some stuff to the navbar and created a sidebar for the manager

--- frontend/src/ui/App.tsx
+++ frontend/src/ui/App.tsx
@@ -1,13 +1,17 @@
 import { HashRouter, Outlet, Route, Routes } from "react-router-dom";
 import pages from "./pages";
+
 import Navbar from "./components/Navbar";
+import ManagerNavbar from "./components/ManagerNavbar";
+import ManagerSidebar from "./components/ManagerSidebar";
+
 import LoginPage from "./pages/login";
 import SignupPage from "./pages/signup";
 import About from "./pages/About";
 import Apply from "./pages/Apply";
 import Dashboard from "./pages/Dashboard";
 
-function Layout() {
+function PublicLayout() {
     return (
         <main className="min-h-screen flex flex-col">
             <Navbar />
@@ -18,21 +22,40 @@ function Layout() {
     );
 }
 
+function ManagerLayout() {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <ManagerNavbar />
+            <div className="flex flex-1">
+                <ManagerSidebar />
+                <main className="flex-1 bg-slate-50 dark:bg-slate-950 p-4">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
+}
+
 function App() {
     return (
         <HashRouter>
             <Routes>
-                <Route element={<Layout />}>
+                
+                <Route element={<PublicLayout />}>
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/signup" element={<SignupPage />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/apply" element={<Apply />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
 
                     {pages.map(({ path, component: Component }) => (
                         <Route key={path} path={path} element={<Component />} />
                     ))}
                 </Route>
+
+                <Route element={<ManagerLayout />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Route>
+
             </Routes>
         </HashRouter>
     );
