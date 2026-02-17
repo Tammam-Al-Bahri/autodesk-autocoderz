592babee4d067597bc278102a23cb13108b55304
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2026-02-17 15:01:26 +0000

    fix(backend): enforce minimum length for first, middle, and last name fields in createUserSchema

--- backend/src/schemas/user.schema.ts
+++ backend/src/schemas/user.schema.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 export const createUserSchema = z.object({
     email: z.email(),
-    firstName: z.string(),
-    middleName: z.string().nullable(),
-    lastName: z.string(),
+    firstName: z.string().min(3),
+    middleName: z.string().min(3).nullable(),
+    lastName: z.string().min(3),
     password: z.string().min(6),
 });
 
