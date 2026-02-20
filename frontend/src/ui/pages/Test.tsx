979c674b915ce9171f6fd1c6410e8c0f80727008
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2026-02-20 00:18:25 +0000

    feat(frontend): update test page

--- frontend/src/ui/pages/Test.tsx
+++ frontend/src/ui/pages/Test.tsx
@@ -1,14 +1,31 @@
 import { LoginForm } from "@/components/login-form";
 import { SignupForm } from "@/components/signup-form";
 import LogoutButton from "@/components/LogoutButton";
+import { useAuth } from "@/context/AuthContext";
+import { Loader } from "lucide-react";
 
 export default function Test() {
-    fetch(`${import.meta.env.VITE_API_BASE_URL}/api/users`);
+    const { user, loading } = useAuth();
+
+    console.log(user);
+
+    if (loading) {
+        return (
+            <div className="fixed inset-0 z-50 flex items-center justify-center h-screen">
+                <Loader className="animate-spin" />
+            </div>
+        );
+    }
+
     return (
         <div>
-            <LoginForm />
-            <LogoutButton />
-            <SignupForm />
+            {user && <LogoutButton />}
+            {!user && (
+                <>
+                    <SignupForm />
+                    <LoginForm />
+                </>
+            )}
         </div>
     );
 }
