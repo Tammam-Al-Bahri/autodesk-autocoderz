d9f463e43ecf46c989e77d2fe8caa8e2594ad001
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2026-03-09 03:45:52 +0000

    fix: frontend apiUrl

--- frontend/src/ui/lib/utils.ts
+++ frontend/src/ui/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export const apiUrl: string = `${import.meta.env.VITE_API_BASE_URL}${base}`;
+export const apiUrl: string = `${import.meta.env.VITE_API_BASE_URL ?? ""}${base}`;
 
 export function formatEnum(value: string) {
     return value
