7695bce159c52e2101d5b7f5de045a4b14599d55
Tammam-Al-Bahri <tammam.al.bahri20@gmail.com>
2026-08-16 01:25:32 +0100

    fix(backend): prisma maria db adapter allowPublicKeyRetrieval true

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,14 +1,15 @@
 import "dotenv/config";
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../generated/prisma/client";
 
 const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-//   connectionLimit: 5
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    allowPublicKeyRetrieval: true,
+    //   connectionLimit: 5
 });
 const prisma = new PrismaClient({ adapter });
 
-export { prisma }
+export { prisma };
